de78195c3828f7720cb9b917a98ab8d6375829fc
Def Paf <warren.y.cheng@gmail.com>
2021-07-22 13:26:07 +0800

    CRV LP Burner

---
+++ tests/fork/Burners/test_curve_lp_burner.py
@@ -0,0 +1,48 @@
+import brownie
+import pytest
+
+from abi.ERC20 import ERC20
+
+
+@pytest.fixture(scope="module")
+def burner(CurveLPBurner, alice, receiver):
+    yield CurveLPBurner.deploy(receiver, receiver, alice, alice, {"from": alice})
+
+
+CrvSETH = "0xa3d87fffce63b53e0d54faa1cc983b7eb0b74a9c"
+CrvSTETH = "0x06325440d014e39736583c165c2963ba99faf14e"
+
+
+@pytest.mark.parametrize("token", [CrvSETH, CrvSTETH])
+def test_burn(MintableTestToken, alice, receiver, burner, token, WETH):
+    token = MintableTestToken.from_abi("test", CrvSETH, abi=ERC20)
+    amount = 10 ** token.decimals()
+
+    token._mint_for_testing(alice, amount, {"from": alice})
+    token.approve(burner, 2 ** 256 - 1, {"from": alice})
+
+    assert token.balanceOf(alice) == amount
+    assert token.balanceOf(burner) == 0
+    assert token.balanceOf(receiver) == 0
+    burner.add_swap_data(CrvSETH)
+    burner.burn(token, {"from": alice})
+
+    assert token.balanceOf(alice) == 0
+    assert token.balanceOf(burner) == 0
+    assert token.balanceOf(receiver) == 0
+
+    assert WETH.balanceOf(alice) == 0
+    assert WETH.balanceOf(burner) == 0
+    assert WETH.balanceOf(receiver) > 0
+
+
+def test_burn_invalid_token(MintableTestToken, alice, burner, receiver):
+    unapproved_token_address = (
+        "0xBfedbcbe27171C418CDabC2477042554b1904857"  # yVault:Curve rETH Pool
+    )
+    unapproved_token = MintableTestToken.from_abi("yVCRVrETH", unapproved_token_address, abi=ERC20)
+    amount = 10 ** unapproved_token.decimals()
+    unapproved_token._mint_for_testing(alice, amount, {"from": alice})
+    # revert when token is not added to burnable_tokens
+    with brownie.reverts("token not burnable"):
+        burner.burn(unapproved_token_address, {"from": alice})
